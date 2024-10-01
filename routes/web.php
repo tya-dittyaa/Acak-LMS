77ca491a7aff326510e7dec3850a6c5d1caa3df2
CW <widjajatjung.charles@gmail.com>
2024-10-01 07:41:09 +0700

    calendar + test database

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('/calendar', function () {
+    return Inertia::render('Calendar');
+});
+
 require __DIR__ . '/auth.php';

6af414ea831c02d93493dd0802b4411b801fc57c
PvAnth <cornelis.antony@gmail.com>
2024-10-02 00:56:36 +0700

    Nambahin dikit dulu kawan2

--- resources/js/Pages/Home.tsx
+++ resources/js/Pages/Home.tsx
@@ -1,30 +1,62 @@
+import MainLayout from "@/Layouts/Main/MainLayout";
+import backGround from "./assets/VectorImage.png";
+
 export default function Home() {
     return (
-        <>
-            <header>HEADER HEADER HEADER</header>
-            <br /><br /><br />
+        <MainLayout title="Home">
+            <section>
+                <div className="flex flex-row items-center justify-center border p-5 space-x-16 bg-gradient-to-r from-cyan-500 to-blue-500">
+                    <div
+                        className="flex h-96 w-96 opacity-40"
+                        style={{
+                            backgroundImage: `url(${backGround})`,
+                            backgroundSize: "cover",
+                            backgroundPosition: "center",
+                        }}
+                    ></div>
+
+                    <div className="flex flex-col w-1/5 space-y-16">
+                        <div className="min-w-fit text-center text-5xl">
+                            <p>
+                                ACAK LMS <br /> Your Unique Learning Experience
+                                <br /> Learning Adventure Starts Here
+                            </p>
+                        </div>
+
+                        <div className="w-auto text-center">
+                            <p>
+                                Frustrated with rigid, overpriced learning
+                                platforms? Discover a fully adaptable,
+                                open-source LMS designed to meet your specific
+                                needs, giving you complete control over your
+                                learning environment.
+                            </p>
+                        </div>
+
+                        <div className="w-auto text-center">
+                            <p>
+                                With 24/7 expert support, top-tier security, and
+                                effortless scalability, our platform evolves
+                                with your needs—crafted with care by a dedicated
+                                team of professionals!
+                            </p>
+                        </div>
+                    </div>
+
+                    <div
+                        className="flex h-96 w-96 opacity-40"
+                        style={{
+                            backgroundImage: `url(${backGround})`,
+                            backgroundSize: "cover",
+                            backgroundPosition: "center",
+                        }}
+                    ></div>
+                </div>
+            </section>
 
             <section>
-                <p>Section 1</p>
-                <h1>Ini Isi Section 1 bakal ada kek Foto website, Tulisan gede, Tulisan kecil (Introduction)</h1>
+                <p>Learning platforms and services made by</p>
             </section>
-            <br /><br /><br />
-
-            <section>
-                <p>Section 2</p>
-                <h1>Ini Isi Section 2 bakal ada tulisan mengenai tugas ini buat apa</h1>
-            </section>
-            <br /><br /><br />
-
-            <section>
-                <p>Section 3</p>
-                <h1>Ini Isi Section 3 bakal ada tulisan mengenai kita ber4 ACAK 😎</h1>
-            </section>
-            <br /><br /><br />
-
-            <footer>
-                Intinya ini footer ada copyright
-            </footer>
-        </>
+        </MainLayout>
     );
 }

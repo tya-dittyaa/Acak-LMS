ed282f65bfd7ca059460b355384f001e412836fe
CW <widjajatjung.charles@gmail.com>
2024-10-06 14:04:53 +0700

    Fetch from DB

--- database/basic.php
+++ database/basic.php
@@ -1,17 +1,22 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header('Content-Type: application/json');
 try {
     $pdo = new PDO('sqlite:AcakLMS.db');
 
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $statement = $pdo->query("SELECT * FROM MsTasks");
+    $statement = $pdo->query("SELECT b.Priority, a.Task, a.CreatedAt, c.MemberName, a.UpdatedAt, a.Deadline, d.Action
+    FROM MsTasks a
+    JOIN MsPriority b ON a.PriorityId = b.PriorityId
+    JOIN MsMember c ON a.MemberId = c.MemberId
+    JOIN MsActions d ON a.ActionId = d.ActionId
+    ");
 
     $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-    echo "<pre>";
-    var_dump($rows);
-    echo "</pre>";
+    echo json_encode($rows);
 } catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
+   echo json_encode(["error" => "Connection failed: " . $e->getMessage()]);
 }
 ?>

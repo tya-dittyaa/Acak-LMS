fb921f955e57d2832933af0a0b1566d7f021b206
Aditya Fajri <aditya.fajri@binus.edu>
2024-10-15 15:55:10 +0700

    refactor: Update PageLayout component to include optional padding

--- resources/js/Layouts/Main/MainLayout.tsx
+++ resources/js/Layouts/Main/MainLayout.tsx
@@ -2,19 +2,34 @@ import { ThemeProvider } from "@/Context/ThemeProvider";
 import FooterLayout from "../Footer/FooterLayout";
 import HeaderLayout from "../Header/HeaderLayout";
 
-interface MainLayoutProps {
+interface PageLayoutProps {
     title: string;
     children: React.ReactNode;
+    hasPadding?: boolean;
 }
 
-export default function MainLayout({ title, children }: MainLayoutProps) {
+export default function PageLayout({
+    title,
+    children,
+    hasPadding = true,
+}: PageLayoutProps) {
     return (
         <div className="flex min-h-screen w-full flex-col">
+            {/* ThemeProvider for handling light/dark mode */}
             <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+                {/* Header component with dynamic title */}
                 <HeaderLayout title={title} />
-                <main className="flex flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-8">
+
+                {/* Main content area with conditional padding */}
+                <main
+                    className={`flex flex-1 flex-col gap-4 bg-muted/40 ${
+                        hasPadding ? "p-4 md:gap-8 md:p-8" : ""
+                    }`}
+                >
                     {children}
                 </main>
+
+                {/* Footer component */}
                 <FooterLayout />
             </ThemeProvider>
         </div>

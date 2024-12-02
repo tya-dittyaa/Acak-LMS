2c14f36565e166f686acc2777c14b5d41f65cb5e
Aditya Fajri <aditya.fajri@binus.edu>
2024-12-02 15:09:48 +0700

    fix: update AuthLayout and AuthForm to use h-full for improved height management

--- resources/js/layouts/Main/AuthLayout.tsx
+++ resources/js/layouts/Main/AuthLayout.tsx
@@ -9,12 +9,12 @@ export default function AuthLayout({ children, useCard = false }: Props) {
     return (
         <div className="flex flex-col lg:flex-row min-h-dvh">
             {/* Left section */}
-            <div className="flex flex-auto h-[15vh] lg:h-dvh justify-center items-center bg-amber-100 lg:w-[40%]">
+            <div className="flex flex-auto h-[15dvh] lg:h-dvh justify-center items-center bg-amber-100 lg:w-[40%]">
                 <ApplicationLogo textClassName="text-4xl lg:text-6xl" />
             </div>
 
             {/* Right section */}
-            <div className="flex flex-col py-5 gap-5 h-[85vh] lg:h-dvh justify-center items-center lg:w-[60%] overflow-hidden">
+            <div className="flex flex-col py-5 gap-5 h-[85dvh] lg:h-dvh justify-center items-center lg:w-[60%] overflow-hidden">
                 <div className="flex flex-col">
                     <h1 className="font-extrabold text-2xl">
                         Welcome to professional

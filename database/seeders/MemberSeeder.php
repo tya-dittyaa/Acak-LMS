1954ec1b0594f3d72e8b568e0681379cf282734b
CW <widjajatjung.charles@gmail.com>
2024-12-10 08:28:10 +0700

    salah seeder

--- database/seeders/MemberSeeder.php
+++ database/seeders/MemberSeeder.php
@@ -14,10 +14,10 @@ class MemberSeeder extends Seeder
     public function run(): void
     {
         DB::table('members')->insert([
-            ['MemberId' => 1, 'MemberName' => 'Charles Widjaja Tjung', 'Email' => 'charles.tjung@binus.ac.id', 'Password' => 'abc123', 'ProfilePicture' => 'https://i.pinimg.com/originals/67/b9/4a/67b94abda90218b077da5762a0df0d2c.jpg'],
-            ['MemberId' => 2, 'MemberName' => 'Aditya Fajri', 'Email' => 'aditya.fajri@binus.ac.id', 'Password' => 'abc123', 'ProfilePicture' => 'https://i.pinimg.com/originals/67/b9/4a/67b94abda90218b077da5762a0df0d2c.jpg'],
-            ['MemberId' => 3, 'MemberName' => 'Karina Dwinovera Mulia', 'Email' => 'karina.mulia@binus.ac.id', 'Password' => 'abc123', 'ProfilePicture' => 'https://i.pinimg.com/originals/67/b9/4a/67b94abda90218b077da5762a0df0d2c.jpg'],
-            ['MemberId' => 4, 'MemberName' => 'Anthony Lawrence Cornelis', 'Email' => 'antony.cornelis@binus.ac.id', 'Password' => 'abc123', 'ProfilePicture' => 'https://i.pinimg.com/originals/67/b9/4a/67b94abda90218b077da5762a0df0d2c.jpg']
+            ['MemberId' => 1, 'MemberName' => 'Charles Widjaja Tjung', 'Email' => 'charles.tjung@binus.ac.id', 'Password' => 'abc123', 'ProfilePicture' => 'gacor kang'],
+            ['MemberId' => 2, 'MemberName' => 'Aditya Fajri', 'Email' => 'aditya.fajri@binus.ac.id', 'Password' => 'abc123', 'ProfilePicture' => 'gacor kang'],
+            ['MemberId' => 3, 'MemberName' => 'Karina Dwinovera Mulia', 'Email' => 'karina.mulia@binus.ac.id', 'Password' => 'abc123', 'ProfilePicture' => 'gacor kang'],
+            ['MemberId' => 4, 'MemberName' => 'Anthony Lawrence Cornelis', 'Email' => 'antony.cornelis@binus.ac.id', 'Password' => 'abc123', 'ProfilePicture' => 'gacor kang']
         ]);
     }
 }
